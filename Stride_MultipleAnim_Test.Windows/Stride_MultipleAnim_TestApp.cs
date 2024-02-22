2da2a71d0c061ff5451eb07fc4b736a648c51d62
Noah7071 <157886157+Noah7071@users.noreply.github.com>
2024-02-22 07:21:02 +0100

    Base commite

---
+++ Stride_MultipleAnim_Test.Windows/Stride_MultipleAnim_TestApp.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace Stride_MultipleAnim_Test
+{
+    class Stride_MultipleAnim_TestApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ Stride_MultipleAnim_Test/AnimationStart.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Animations;
+using Stride.Input;
+using Stride.Engine;
+
+namespace Stride_MultipleAnim_Test
+{
+    [DataContract("PlayAnimation")]
+    public class PlayAnimation
+    {
+        public AnimationClip Clip;
+        public AnimationBlendOperation BlendOperation = AnimationBlendOperation.LinearBlend;
+        public double StartTime = 0;
+    }
+
+    /// <summary>
+    /// Script which starts a few animations on its entity
+    /// </summary>
+    public class AnimationStart : StartupScript
+    {
+        /// <summary>
+        /// A list of animations to be loaded when the script starts
+        /// </summary>
+        public readonly List<PlayAnimation> Animations = new List<PlayAnimation>();
+
+        public override void Start()
+        {
+            var animComponent = Entity.GetOrCreate<AnimationComponent>();
+
+  //          animComponent.Play("rotate");
+
+animComponent.Play("updown");
+//animComponent.Play("sideways");
+        }
+
+        private void PlayAnimations(AnimationComponent animComponent)
+        {
+            foreach (var anim in Animations)
+            {
+                if (anim.Clip != null)
+                    animComponent.Add(anim.Clip, anim.StartTime, anim.BlendOperation);
+            }
+
+            Animations.Clear();
+        }
+    }
+}
